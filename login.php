de49bd5d9b94e575013303bf0f85f50b82ab4642
andhikaputraaaa <andikaputrari@gmail.com>
2024-11-21 22:03:15 +0700

    login

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="form-container">
+        <img src="assets\chicken.png" class="logo">
+        <form action="post">
+            <div class="form-group">
+            <label for="email"><span class="icon">👤</span></label>
+            <input type="text" placeholder="Input your email here">
+            </div>
+            <div class="form-group">
+            <label for="password"><span class="icon">🔒</span></label>
+            <input type="password" placeholder="Input your password here">
+            </div>
+            <button type="submit" class="btn">Sign In</button>
+        </form>
+    </div>
+</body>
+</html>
