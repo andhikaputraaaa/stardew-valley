1c7dd9803fb0494632b49b17d1e4bb1bd8680982
andhikaputraaaa <andikaputrari@gmail.com>
2024-11-21 13:09:29 +0700

    halaman awal

--- index.php
+++ index.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
 </head>
 <body>
-    coba
+    <div class="container">
+        <h1>What Villagers Are You? <img src="assets\play_arrow_filled.png" class="logo"> </h1>
+        <div class="buttons">
+            <button class="btn">Sign Up</button>
+            <button class="btn">Sign In</button>
+        </div>
+    </div>
 </body>
 </html>
