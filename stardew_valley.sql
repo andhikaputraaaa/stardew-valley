d5b999726ca4a5c7bb415ba0c6d10d03c3f8b55a
wildannmh <wildan.habib@mhs.unsoed.ac.id>
2024-12-15 13:29:16 +0700

    stardew_valley.sql update

--- stardew_valley.sql
+++ stardew_valley.sql
@@ -1,47 +1,103 @@
--- buat bikin databasenya
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2024 at 07:27 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
-CREATE DATABASE stardew_valley;
-
-USE stardew_valley;
-
-CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    role varchar(10) NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    gender ENUM('Male', 'Female') NOT NULL
-);
-
-INSERT INTO user (role, first_name, last_name, email, password, gender) VALUES ('mayor', 'mayo', 'r', 'mayor@gmail.com', '12345678', 'Male')
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE hasil_mancing (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    image VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- CREATE TABLE jenis_ikan (
---     id INT AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(100) NOT NULL,
---     image VARCHAR(255) NOT NULL
--- );
+--
+-- Database: `stardew_valley`
+--
 
--- INSERT INTO jenis_ikan (name, image) VALUES
--- ('Carp', 'assets/Carp.png'),
--- ('Catfish', 'assets/Catfish.png'),
--- ('Eel', 'assets/Eel.png'),
--- ('Lava Eel', 'assets/Lava_Eel.png'),
--- ('Octopus', 'assets/Octopus.png'),
--- ('Pufferfish', 'assets/Pufferfish.png'),
--- ('Salmon', 'assets/Salmon.png'),
--- ('Sandfish', 'assets/Sandfish.png'),
--- ('Sardine', 'assets/Sardine.png'),
--- ('Squid', 'assets/Squid.png'),
--- ('Stonefish', 'assets/Stonefish.png'),
--- ('Super Cucumber', 'assets/Super_Cucumber.png'),
--- ('Tuna', 'assets/Tuna.png');
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hasil_mancing`
+--
+
+CREATE TABLE `hasil_mancing` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `role` varchar(10) NOT NULL DEFAULT 'villager',
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `gender` enum('Male','Female','I prefer not to say') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `hasil_mancing`
+--
+ALTER TABLE `hasil_mancing`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `hasil_mancing`
+--
+ALTER TABLE `hasil_mancing`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `hasil_mancing`
+--
+ALTER TABLE `hasil_mancing`
+  ADD CONSTRAINT `hasil_mancing_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
